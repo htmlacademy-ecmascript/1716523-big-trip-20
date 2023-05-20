c84cb93824d351379037f57bbce19aecdc3204c4
Vladimir <yaschuk_vladimir@mail.ru>
2023-05-20 23:45:08 +0300

    creates filters and addNewEventButton

--- src/mock/generate-point-mock.js
+++ src/mock/generate-point-mock.js
@@ -4,8 +4,8 @@ function generatePoint (pointType, destinationId, offersIds) {
   return {
     id: getRandomInteger(1, 10),
     basePrice: getRandomNumber(),
-    dateFrom: '2019-07-10T22:55:56.845Z',
-    dateTo: '2019-07-11T11:22:13.375Z',
+    dateFrom: `2024-0${getRandomInteger(1, 9)}-10T22:55:56.845Z`,
+    dateTo: `2024-0${getRandomInteger(1, 9)}-11T11:22:13.375Z`,
     destination: destinationId,
     isFavorite: !!getRandomInteger(0, 1),
     offers: offersIds,
--- src/trip-events-presenter.js
+++ src/trip-events-presenter.js
@@ -5,20 +5,24 @@ import TripEventsItemView from './view/trip-events-item-view.js';
 import PhotoeTemplate from './view/photo-view';
 import PhotoesContainer from './view/event-photoes-container-view';
 import NoEvents from './view/no-events-view';
-import HeaderFiltersView from './view/header-form-view.js';
+// import HeaderFiltersView from './view/header-form-view.js';
 import TripInfoView from './view/trip-info-view.js';
 import TripSortView from './view/trip-sort-view.js';
 import FormWithoutDestination from './view/event-without-destination-view';
+
+// import FilterPresenter from './filter-presenter';
 
 
 const siteMainElement = document.querySelector('.page-main');
 const tripEventsElement = siteMainElement.querySelector('.trip-events');
 const siteHeaderElement = document.querySelector('.page-header');
 const siteTripInfoElement = siteHeaderElement.querySelector('.trip-main');
-const siteFiltersElement = siteHeaderElement.querySelector('.trip-controls__filters');
+// const siteFiltersElement = siteHeaderElement.querySelector('.trip-controls__filters');
+
+const addNewEventButton = document.querySelector('.trip-main__event-add-btn');
 
 export default class TripEventsListPresenter {
-  #tripEventsList = new TripEventsListView();
+  tripEventsList = new TripEventsListView();
   #photoesContainer = new PhotoesContainer();
 
   constructor({listContainer, pointsModel}) {
@@ -34,10 +38,15 @@ export default class TripEventsListPresenter {
 
     render (new TripInfoView, siteTripInfoElement, RenderPosition.AFTERBEGIN);
     render (new TripSortView, tripEventsElement);
-    render (new HeaderFiltersView, siteFiltersElement);
+    // render (new HeaderFiltersView, siteFiltersElement);
 
     this.#renderPoints();
 
+    const addNewEvent = () => {
+      render(new FormWithoutDestination(), this.tripEventsList.element, RenderPosition.AFTERBEGIN);
+    };
+
+    addNewEventButton.addEventListener('click', addNewEvent);
   }
 
   #renderPoints() {
@@ -45,7 +54,7 @@ export default class TripEventsListPresenter {
       const noEventsComponent = new NoEvents;
       render (noEventsComponent, this.listContainer);
     } else {
-      render (this.#tripEventsList, this.listContainer);
+      render (this.tripEventsList, this.listContainer);
       this.points.forEach((point, i) => {
         this.#renderPoint(this.offers, point, this.destinations[i]);
       });
@@ -58,9 +67,9 @@ export default class TripEventsListPresenter {
     const currentOffer = offers.find((offer) => offer.type === point.type);
     const itemComponent = new TripEventsItemView({point}, {...currentOffer}, destination, showFormElement);
     const editItemComponent = new TripEventsEditItemView({point}, {...currentOffer}, {...destination},
-      submitFormElement, hideFormElement, resetForm, addNewEvent);
+      submitFormElement, hideFormElement, resetForm);
     render (new PhotoeTemplate(destination), this.#photoesContainer.element);
-    render (itemComponent, this.#tripEventsList.element);
+    render (itemComponent, this.tripEventsList.element);
 
 
     const escKeyDownHandler = (evt) => {
@@ -70,10 +79,6 @@ export default class TripEventsListPresenter {
         document.removeEventListener('keydown', escKeyDownHandler);
       }
     };
-
-    function addNewEvent() {
-      render(new FormWithoutDestination(), this.listContainer);
-    }
 
     function showFormElement() {
       replaceCardToForm();
